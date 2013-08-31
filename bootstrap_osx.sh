b741baaec305935ed918f114fde4d5a17192b160
Michael van de Waeter <mvdwaeter@gmail.com>
2013-08-31 11:50:46 +0200

    Added ZSH shell

--- bootstrap_osx.sh
+++ bootstrap_osx.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+echo "git clone .oh-my-zsh"
+[ ! -f ~/.oh-my-zsh ] &&  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.dotfiles/.oh-my-zsh
+
+echo "set zsh as default shell"
+chsh -s /bin/zsh
+
+echo "symlink .zshrc"
+[ ! -f ~/.zshrc ] && ln -s ~/.dotfiles/common/.zshrc ~/.zshrc
 
 echo "symlink osx .bash_profile"
 [ ! -f ~/.bash_profile ] && ln -s ~/.dotfiles/osx/.bash_profile ~/.bash_profile
